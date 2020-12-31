b13ea54682b471cc6eb5e4ecdee337867f638890
Sandra Juárez <68796014+SandGG@users.noreply.github.com>
2020-12-30 22:10:06 -0600

    Update main.go

--- main.go
+++ main.go
@@ -2,47 +2,34 @@ package main
 
 import "fmt"
 
-type (
-	height int
-	width  int
-	area   int
-)
+type search func(string) string
 
-var (
-	vWidth, vHeight int
-	vArea           area
-)
-
-func main() {
-	var area = func(pWidth width, pHeight height) area {
-		vWidth = int(pWidth)
-		vHeight = int(pHeight)
-		vArea = area(vHeight * vWidth)
-		return vArea
-	}
-
-	var inc = func(pWidth *width, pHeight *height) (width, height) {
-		*pWidth += 15
-		*pHeight += 15
-		return *pWidth, *pHeight
-	}
-
-	var ww width = 10
-	var hh height = 5
-
-	fmt.Println(inc(&ww, &hh))
-	fmt.Println(side(inc))
-	fmt.Println("With area and increment:", area(inc(side(inc))))
-
+type person struct {
+	name string
+	age  int
 }
 
-func side(fn func(pWidth *width, pHeight *height) (width, height)) (*width, *height) {
-	var vHeight height
-	var vWidth width
-	fmt.Println("Enter width")
-	fmt.Scan(&vWidth)
-	fmt.Println("Enter height")
-	fmt.Scan(&vHeight)
+func main() {
+	fmt.Println(nameSearch(writeData())("Romina"))
+}
 
-	return &vWidth, &vHeight
+func nameSearch(per []person) search {
+	return func(str string) string {
+		for _, v := range per {
+			if str == v.name {
+				return str + " found"
+			}
+		}
+		return str + " not found"
+	}
+}
+
+func writeData() []person {
+	var people = []person{
+		{name: "Maria", age: 13},
+		{name: "Rosa", age: 29},
+		{name: "Laura", age: 30},
+		{name: "Juan", age: 25},
+	}
+	return people
 }
